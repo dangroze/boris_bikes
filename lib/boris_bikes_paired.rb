b6a7a397eb4253aaaf491cba60f6285362c6bcf6
Dan Groze <dangroze@Dans-MBP.home>
2019-01-08 21:35:47 +0000

    Step10

--- lib/boris_bikes_paired.rb
+++ lib/boris_bikes_paired.rb
@@ -1,9 +1,10 @@
 class DockingStation
   def release_bike
-
+    return Bike.new
     end
 end
 class Bike
   def working?
+    true
   end
 end
--- spec/boris_bikes_paired_spec.rb
+++ spec/boris_bikes_paired_spec.rb
@@ -1,5 +1,18 @@
 require "boris_bikes_paired"
+require 'spec_helper'
 
 describe DockingStation do
 	it {is_expected.to respond_to(:release_bike)}
+  describe "release_bike" do
+		it "returns a bike" do
+			subject.release_bike
+		end
+	#it "releases a bike" do
+	#	DockingStation.new.release_bike
+	end
+	describe "working?" do
+		it "is in working order or not" do
+			expect(subject.release_bike).to be_working
+		end
+	end
 end
