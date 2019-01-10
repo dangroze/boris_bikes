72a93eb59e8b437ff334b8871af0b880ccf20720
dangroze <dangroze@gmail.com>
2019-01-10 10:52:43 +0000

    Step11 + refactoring

--- lib/boris_bikes_paired.rb
+++ lib/boris_bikes_paired.rb
@@ -1,9 +1,17 @@
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     return Bike.new
     end
+
+  def dock(bike)
+    @bike = bike
+  end
 end
+
 class Bike
+
   def working?
     true
   end
--- spec/boris_bikes_paired_spec.rb
+++ spec/boris_bikes_paired_spec.rb
@@ -1,18 +1,18 @@
 require "boris_bikes_paired"
-require 'spec_helper'
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
-  describe "release_bike" do
-		it "returns a bike" do
-			expect(subject.release_bike) == Bike.new
-		end
-	#it "releases a bike" do
-	#	DockingStation.new.release_bike
+	it "returns a bike" do
+		expect(subject.release_bike) == Bike.new
+	  # subject = DockingStation.new
 	end
-	describe "working?" do
-		it "is in working order or not" do
-			expect(subject.release_bike).to be_working
-		end
+	it "is in working order or not" do
+		expect(subject.release_bike).to be_working
+	end
+	it {is_expected.to respond_to(:dock).with(1).argument}
+	it 'returns docked bikes' do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.bike) == bike
 	end
 end
