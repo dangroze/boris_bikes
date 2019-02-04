002bb08aaa0b6ce0b8c65cde75a079526ac0eed9
Dan Groze <dangroze@gmail.com>
2019-02-04 21:07:12 +0000

    Added test for not releasing broken bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -6,11 +6,16 @@ describe DockingStation do
 		it "raises an error when there are no bikes" do
 			expect { subject.release_bike }.to raise_error "NoBikeError"
 		end
-	  it "releases a bike" do
-      bike = double('a bike')
+	  it "releases a working bike" do
+      bike = double(:bike, :broken? => false)
 		  subject.dock(bike)
 		  expect(subject.release_bike).to eq bike
 	  end
+		it 'does not release broken bike' do
+      bike = double(:bike, :broken? => true)
+      subject.dock(bike)
+      expect(subject.release_bike).to_not eq(bike)
+    end
 	end
 	describe '#dock' do
 		it "raises an error when dock is full" do
